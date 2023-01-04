5385e7b89c54dff42f10364deef5b671da50bd60
RemagOfficial <52126530+RemagOfficial@users.noreply.github.com>
2023-01-04 20:53:32 +0000

    made the green and blue screen blocks emit light

--- src/main/java/com/remag/greenscreen/GreenScreen.java
+++ src/main/java/com/remag/greenscreen/GreenScreen.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
@@ -14,16 +13,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class GreenScreen implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-
-	public static final Block GREENSCREEN = new Block(FabricBlockSettings.of(Material.WOOL).strength(4.0f));
-	public static final Block BLUESCREEN = new Block(FabricBlockSettings.of(Material.WOOL).strength(4.0f));
+	public static final Block GREENSCREEN = new Block(FabricBlockSettings.of(Material.WOOL).strength(4.0f).luminance(15));
+	public static final Block BLUESCREEN = new Block(FabricBlockSettings.of(Material.WOOL).strength(4.0f).luminance(15));
 	public static final ItemGroup GREEN_SCREEN = FabricItemGroup.builder(
 					new Identifier("greenscreen", "green_screen"))
 			.icon(() -> new ItemStack(GreenScreen.GREENSCREEN))
